ad7fe7cb796eff67e9fa6ebdf9bb69717eae9591
Василь Колотюк <vasakolotuk02@gmail.com>
2020-12-07 21:28:44 +0200

    first commit

---
+++ home-work_disizn/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace home_work_disizn
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
